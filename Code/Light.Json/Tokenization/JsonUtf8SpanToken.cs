f61fcdef6cf09eecd0c65cada4df2da76fd8e078
Kenny Pflug <kenny.pflug@live.de>
2019-07-15 20:50:25 +0200

    added JsonUtf8SpanToken

---
+++ Code/Light.Json/Tokenization/JsonUtf8SpanToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Light.Json.FrameworkExtensions;
+
+namespace Light.Json.Tokenization
+{
+    public readonly ref struct JsonUtf8SpanToken
+    {
+        public readonly JsonTokenType Type;
+        public readonly ReadOnlySpan<byte> Text;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public JsonUtf8SpanToken(JsonTokenType type, ReadOnlySpan<byte> text = default)
+        {
+            Type = type;
+            Text = text;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Equals(JsonUtf8SpanToken other) =>
+            Type == other.Type && (Text == other.Text || Text.SequenceEqual(other.Text));
+
+        public override bool Equals(object obj) =>
+            throw BoxingNotSupported.CreateException();
+
+        public override int GetHashCode() =>
+            throw BoxingNotSupported.CreateException();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(JsonUtf8SpanToken x, JsonUtf8SpanToken y) => x.Equals(y);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(JsonUtf8SpanToken x, JsonUtf8SpanToken y) => !x.Equals(y);
+    }
+}
