05937c83d0e8aa212d3199d177635a7f96f55d96
Kenny Pflug <kenny.pflug@live.de>
2019-08-23 10:20:22 +0200

    added test for ignoring single line comments in Utf8Tokenizer

--- Code/Light.Json.Tests/Tokenization/Utf8/JsonUtf8TokenizerTests.cs
+++ Code/Light.Json.Tests/Tokenization/Utf8/JsonUtf8TokenizerTests.cs
@@ -206,6 +206,36 @@ namespace Light.Json.Tests.Tokenization.Utf8
             tokenizer.GetNextToken().ShouldEqual("", JsonTokenType.EndOfDocument);
         }
 
+        [Fact]
+        public static void IgnoreSingleLineComments()
+        {
+            const string json = @"
+// This is a single line comment at the beginning of the file
+{
+    ""someCollection"": [
+        // Here is a comment within a collection
+        42,
+        18,
+        30
+    ]
+}";
+            var jsonInUtf8 = json.ToUtf8();
+            var tokenizer = new JsonUtf8Tokenizer(jsonInUtf8);
+
+            tokenizer.GetNextToken().ShouldEqual("{", JsonTokenType.BeginOfObject);
+            tokenizer.GetNextToken().ShouldEqual("someCollection", JsonTokenType.String);
+            tokenizer.GetNextToken().ShouldEqual(":", JsonTokenType.NameValueSeparator);
+            tokenizer.GetNextToken().ShouldEqual("[", JsonTokenType.BeginOfArray);
+            tokenizer.GetNextToken().ShouldEqual("42", JsonTokenType.IntegerNumber);
+            tokenizer.GetNextToken().ShouldEqual(",", JsonTokenType.EntrySeparator);
+            tokenizer.GetNextToken().ShouldEqual("18", JsonTokenType.IntegerNumber);
+            tokenizer.GetNextToken().ShouldEqual(",", JsonTokenType.EntrySeparator);
+            tokenizer.GetNextToken().ShouldEqual("30", JsonTokenType.IntegerNumber);
+            tokenizer.GetNextToken().ShouldEqual("]", JsonTokenType.EndOfArray);
+            tokenizer.GetNextToken().ShouldEqual("}", JsonTokenType.EndOfObject);
+            tokenizer.GetNextToken().ShouldEqual("", JsonTokenType.EndOfDocument);
+        }
+
         private static void TestTokenizer(string json, JsonTokenType expectedTokenType) =>
             GetSingleToken(json).ShouldEqual(json.Trim(), expectedTokenType);
 
