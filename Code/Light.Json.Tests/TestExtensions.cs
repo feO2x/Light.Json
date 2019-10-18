352e37086433a9de40f6312852298b334c86d83c
Kenny Pflug <kenny.pflug@live.de>
2019-10-18 12:52:02 +0200

    removed aggressive inlining in test extensions

--- Code/Light.Json.Tests/TestExtensions.cs
+++ Code/Light.Json.Tests/TestExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using Light.Json.Tokenization.Utf8;
 using Xunit.Sdk;
 
@@ -7,7 +6,6 @@ namespace Light.Json.Tests
 {
     public static class TestExtensions
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MustEqual(in this ReadOnlySpan<char> span, in ReadOnlySpan<char> other)
         {
             if (!span.Equals(other, StringComparison.Ordinal))
@@ -17,7 +15,6 @@ namespace Light.Json.Tests
         private static void ThrowTextSpansNotEqualException(in this ReadOnlySpan<char> span, in ReadOnlySpan<char> other) =>
             throw new XunitException($"Expected \"{span.ToString()}\" to be equal to \"{other.ToString()}\", but it is not.");
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MustEqual(in this ReadOnlySpan<byte> span, in ReadOnlySpan<byte> other)
         {
             if (!span.SequenceEqual(other))
@@ -27,7 +24,6 @@ namespace Light.Json.Tests
         private static void ThrowByteSpansNotEqualException(in ReadOnlySpan<byte> span, in ReadOnlySpan<byte> other) =>
             throw new XunitException($"Expected byte sequence \"{span.ToString()}\" to be equal to \"{other.ToString()}\", but it is not.");
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void MustBeDefault(this in Utf8Character character)
         {
             if (!character.Equals(default(Utf8Character)))
