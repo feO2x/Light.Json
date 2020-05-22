4c512d45aa8c23f7d7ea23a7cd157ebd5db133b9
Kenny Pflug <kenny.pflug@live.de>
2020-05-22 13:26:17 +0200

    added benchmark for simple mutable DTO serialization

---
+++ Code/Light.Json.Performance/SerializationBenchmarks/SerializeSimpleMutableDtoUtf16Benchmark.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using Light.Json.Contracts;
+using Light.Json.Serialization.LowLevelWriting;
+using Light.Json.Tests.Serialization;
+using Light.Json.Tests.SerializationSubjects;
+using Newtonsoft.Json;
+using LightJsonSerializer = Light.Json.JsonSerializer;
+using SystemTextJsonSerializer = System.Text.Json.JsonSerializer;
+using Utf8JsonSerializer = Utf8Json.JsonSerializer;
+
+namespace Light.Json.Performance.SerializationBenchmarks
+{
+    public class SerializeSimpleMutableDtoUtf16Benchmark
+    {
+        public LightJsonSerializer LightJsonSerializer;
+        public static readonly Person Person = new Person { FirstName = "Kenny", LastName = "Pflug", Age = 33 };
+
+        [GlobalSetup(Target = nameof(LightJson))]
+        public void SetupLightJsonSerializer()
+        {
+            LightJsonSerializer = new LightJsonSerializer(
+                new ImmutableSerializationContractProvider(
+                    new Dictionary<TypeKey, ISerializationContract>
+                    {
+                        [typeof(Person)] = new SerializeSimpleMutableObjectTests.PersonContract()
+                    }
+                ),
+                new ArrayPoolBufferProvider<char>(),
+                new ArrayPoolBufferProvider<byte>()
+            );
+        }
+
+        [Benchmark(Baseline = true)]
+        public string JsonNet() => JsonConvert.SerializeObject(Person);
+
+        [Benchmark]
+        public string LightJson() => LightJsonSerializer.SerializeToUtf16(Person);
+
+        [Benchmark]
+        public string SystemTextJson() => SystemTextJsonSerializer.Serialize(Person);
+
+        [Benchmark]
+        public string Utf8Json() => Utf8JsonSerializer.ToJsonString(Person);
+    }
+}
---
+++ Code/Light.Json.Performance/SerializationBenchmarks/SerializeSimpleMutableDtoUtf8Benchmark.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Light.Json.Contracts;
+using Light.Json.Serialization.LowLevelWriting;
+using Light.Json.Tests.Serialization;
+using Light.Json.Tests.SerializationSubjects;
+using JsonNetSerializer = Newtonsoft.Json.JsonSerializer;
+using LightJsonSerializer = Light.Json.JsonSerializer;
+using SystemTextJsonSerializer = System.Text.Json.JsonSerializer;
+using Utf8JsonSerializer = Utf8Json.JsonSerializer;
+
+namespace Light.Json.Performance.SerializationBenchmarks
+{
+    public class SerializeSimpleMutableDtoUtf8Benchmark
+    {
+        public JsonNetSerializer JsonNetSerializer;
+        public LightJsonSerializer LightJsonSerializer;
+        public static readonly Person Person = new Person { FirstName = "Kenny", LastName = "Pflug", Age = 33 };
+
+        [GlobalSetup(Target = nameof(JsonNet))]
+        public void SetupJsonNet()
+        {
+            JsonNetSerializer = JsonNetSerializer.CreateDefault();
+        }
+
+        [GlobalSetup(Target = nameof(LightJson))]
+        public void SetupLightJson()
+        {
+            LightJsonSerializer = new LightJsonSerializer(
+                new ImmutableSerializationContractProvider(
+                    new Dictionary<TypeKey, ISerializationContract>
+                    {
+                        [typeof(Person)] = new SerializeSimpleMutableObjectTests.PersonContract()
+                    }
+                ),
+                new ArrayPoolBufferProvider<char>(),
+                new ArrayPoolBufferProvider<byte>()
+            );
+        }
+
+        [Benchmark(Baseline = true)]
+        public byte[] JsonNet()
+        {
+            using var memoryStream = new MemoryStream();
+            using var streamWriter = new StreamWriter(memoryStream, Encoding.UTF8);
+            JsonNetSerializer.Serialize(streamWriter, Person);
+            return memoryStream.ToArray();
+        }
+
+        [Benchmark]
+        public byte[] LightJson() => LightJsonSerializer.SerializeToUtf8(Person);
+
+        [Benchmark]
+        public byte[] SystemTextJson() => SystemTextJsonSerializer.SerializeToUtf8Bytes(Person);
+
+        [Benchmark]
+        public byte[] Utf8Json() => Utf8JsonSerializer.Serialize(Person);
+    }
+}
