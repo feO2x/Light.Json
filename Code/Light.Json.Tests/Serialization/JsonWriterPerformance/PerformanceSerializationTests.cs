f3ddb7b057c9d42c9fa90dbc4c515a841841fffb
Kenny Pflug <kenny.pflug@live.de>
2020-06-06 14:59:34 +0200

    added Contract

--- Code/Light.Json.Tests/Serialization/JsonWriterPerformance/PerformanceSerializationTests.cs
+++ Code/Light.Json.Tests/Serialization/JsonWriterPerformance/PerformanceSerializationTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Light.Json.Buffers;
 using Light.Json.FrameworkExtensions;
 using Light.Json.Tests.SerializationSubjects;
@@ -10,10 +8,8 @@ namespace Light.Json.Tests.Serialization.JsonWriterPerformance
 {
     public static class PerformanceSerializationTests
     {
-        public static readonly byte[] Segment1 = Encoding.UTF8.GetBytes("{\"firstName\":");
-        public static readonly byte[] Segment2 = Encoding.UTF8.GetBytes(",\"lastName\":");
-        public static readonly byte[] Segment3 = Encoding.UTF8.GetBytes(",\"age\":");
         public static readonly IBufferProvider<byte> BufferProvider = new ThreadStaticByteBufferProvider();
+        public static readonly PersonContract Contract = new PersonContract();
 
         [Fact]
         public static void SerializeUtf8()
@@ -28,14 +24,7 @@ namespace Light.Json.Tests.Serialization.JsonWriterPerformance
         public static SerializationResult<byte> Serialize(Person person)
         {
             var writer = new JsonWriter(BufferProvider);
-            writer.WriteRaw(Segment1);
-            writer.WriteString(person.FirstName.AsSpan());
-            writer.WriteRaw(Segment2);
-            writer.WriteString(person.LastName.AsSpan());
-            writer.WriteRaw(Segment3);
-            writer.WriteNumber(person.Age);
-            writer.WriteEndOfObject();
-
+            Contract.Serialize(person, ref writer);
             return writer.GetUtf8Json();
         }
     }
---
+++ Code/Light.Json.Tests/Serialization/JsonWriterPerformance/PersonContract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using Light.Json.Tests.SerializationSubjects;
+
+namespace Light.Json.Tests.Serialization.JsonWriterPerformance
+{
+    public sealed class PersonContract
+    {
+        public static readonly byte[] Segment1 = Encoding.UTF8.GetBytes("{\"firstName\":");
+        public static readonly byte[] Segment2 = Encoding.UTF8.GetBytes(",\"lastName\":");
+        public static readonly byte[] Segment3 = Encoding.UTF8.GetBytes(",\"age\":");
+
+        public void Serialize(Person person, ref JsonWriter writer)
+        {
+            writer.WriteRaw(Segment1);
+            writer.WriteString(person.FirstName.AsSpan());
+            writer.WriteRaw(Segment2);
+            writer.WriteString(person.LastName.AsSpan());
+            writer.WriteRaw(Segment3);
+            writer.WriteNumber(person.Age);
+            writer.WriteEndOfObject();
+        }
+    }
+}
