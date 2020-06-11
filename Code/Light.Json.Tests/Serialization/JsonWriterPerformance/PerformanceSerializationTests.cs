ea6dfc959a9d5ef9f4c2378bda599a04d203f0f1
Kenny Pflug <kenny.pflug@live.de>
2020-06-11 12:12:38 +0200

    added generic method that checks for contracts without a dedicated key

--- Code/Light.Json.Tests/Serialization/JsonWriterPerformance/PerformanceSerializationTests.cs
+++ Code/Light.Json.Tests/Serialization/JsonWriterPerformance/PerformanceSerializationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using FluentAssertions;
 using Light.Json.Buffers;
@@ -12,14 +13,13 @@ namespace Light.Json.Tests.Serialization.JsonWriterPerformance
     {
         public static readonly IBufferProvider<byte> BufferProvider = new ThreadStaticByteBufferProvider();
 
-        public static readonly ContractsDictionary Contracts =
-            new ContractsDictionary().Add(new PersonContract());
+        public static readonly PersonContract PersonContract = new PersonContract();
 
-        //public static readonly Dictionary<TypeKey, BaseContract> Contracts =
-        //    new Dictionary<TypeKey, BaseContract>(TypeKey.EqualityComparer.Instance)
-        //    {
-        //        [typeof(Person)] = new PersonContract()
-        //    };
+        public static readonly Dictionary<TypeKey, BaseContract> Contracts =
+            new Dictionary<TypeKey, BaseContract>(TypeKey.EqualityComparer.Instance)
+            {
+                [typeof(Person)] = new PersonContract()
+            };
 
         [Fact]
         public static void SerializeUtf8()
@@ -31,15 +31,24 @@ namespace Light.Json.Tests.Serialization.JsonWriterPerformance
             result.Json.Span.ConvertFromUtf8ToString().Should().Be(Person.DefaultMinifiedJson);
         }
 
-        public static SerializationResult<byte> Serialize(Person person)
+        public static SerializationResult<byte> Serialize<T>(T value)
         {
-            if (!Contracts.TryGetContract<ISerializeOnlyContract<Person>>(typeof(Person), out var contract))
-                throw new SerializationException();
-
             var writer = new JsonWriter(BufferProvider);
             var context = new SerializationContext();
-            contract.Serialize(person, context, ref writer);
+            SerializeViaContract(value, context, ref writer);
             return writer.GetUtf8Json();
+        }
+
+        private static void SerializeViaContract<T, TJsonWriter>(T value, SerializationContext context, ref TJsonWriter writer)
+            where TJsonWriter : struct, IJsonWriter
+        {
+            if (typeof(T) == typeof(Person))
+            {
+                PersonContract.Serialize(Unsafe.As<T, Person>(ref value), context, ref writer);
+                return;
+            }
+
+            throw new SerializationException("Unknown contract type");
         }
     }
 }
