1169a573e1144f4231eb2d6321ab937c94cad4a3
Kenny Pflug <kenny.pflug@live.de>
2020-08-16 14:07:54 +0200

    added own SerializationException

---
+++ Code/Light.Json/SerializationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Light.Json
+{
+    [Serializable]
+    public class SerializationException : System.Runtime.Serialization.SerializationException
+    {
+        public SerializationException(string? message = null, Exception? innerException = null) : base(message, innerException) { }
+
+        protected SerializationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
